a6276dfa54fa30a45eebbb296346a0d433691c95
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2021-10-02 19:41:58 +0200

    Boss testing

--- Assets/DamageCollider.cs
+++ Assets/DamageCollider.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class DamageCollider : MonoBehaviour
 {
+    [SerializeField] private float damageAmount = 3.1f;
+
+    [SerializeField] private bool onCollision = true;
+    [SerializeField] private bool onTrigger = true;
     private void OnCollisionEnter2D(Collision2D other)
     {
+        if(!onCollision) return;
+
         IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
 
         if(damageable is IDamageable)
         {
-            damageable.OnDamage(0.3f);
+            damageable.OnDamage(damageAmount);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(!onTrigger) return;
+        
+        IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
+
+        if(damageable is IDamageable)
+        {
+            damageable.OnDamage(damageAmount);
         }
     }
 }
